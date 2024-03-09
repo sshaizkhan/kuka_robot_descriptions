83fc5cff900c81551a40ed873df7f5e21a75b8a3
Shahwaz Khan <127909724+shahwazk@users.noreply.github.com>
2024-03-08 17:48:59 -0800

    Add KR560 R3100 2 robot model (#8)
    * Add kinematics, joint limits, physical parameters, and visual parameters for KR560 R3100 2 robot.
    
    * Add new robot model options to view_kuka.launch.py

--- launch/view_kuka.launch.py
+++ launch/view_kuka.launch.py
@@ -46,7 +46,7 @@ def generate_launch_description():
             description="Type/series of used UR robot.",
             choices=["kr6_r700_sixx", "kr6_r900_sixx",
                      "kr10_r1100_2", "kr16_r2010_2", "kr210_r2700_2",
-                     "kr210_r3100_2"],
+                     "kr210_r3100_2", "kr560_r3100_2", ],
         )
     )
     # General arguments
